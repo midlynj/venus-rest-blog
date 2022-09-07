0a098ff0ac71c661f898d02ecc7a6e95acc8ff50
midlynj <jmidlyn@gmail.com>
2022-09-07 16:52:58 -0500

    create post method created

--- src/main/java/stacks/venusrestblog/PostController.java
+++ src/main/java/stacks/venusrestblog/PostController.java
@@ -1,8 +1,6 @@
 package stacks.venusrestblog;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import stacks.venusrestblog.data.Post;
 
 import java.util.ArrayList;
@@ -11,24 +9,32 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 public class PostController {
-
+    private List<Post> posts = new ArrayList<>();
     @GetMapping("/")
     public List<Post> fetchPosts() {
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L,"Post 1","This is my first post"));
-        posts.add(new Post(2L,"Post 2", "This is my second post"));
+
+//        posts.add(new Post(1L,"Post 1","This is my first post"));
+//        posts.add(new Post(2L,"Post 2", "This is my second post"));
         return posts;
     }
 
-    public Post fetchPostsById(long id) {
+    @GetMapping("/{id}")
+    public Post fetchPostsById(@PathVariable long id) {
         switch ((int) id) {
             case 1:
                 return new Post(1L,"Post 1","This is my first post");
             case 2:
-                new Post(2L,"Post 2", "This is my second post");
+                return new Post(2L,"Post 2", "This is my second post");
             default:
-                return "Not found";
+               throw new RuntimeException("Error");
         }
+
+    }
+
+    @PostMapping("/newPost")
+    public void createPost(@RequestBody Post myStuff) {
+
+      posts.add(myStuff);
 
     }
 }
